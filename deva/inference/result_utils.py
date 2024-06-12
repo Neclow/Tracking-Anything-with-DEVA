036f43a73042815c7c187cc3ff07c266c4569e5a
Neil Scheidwasser <32112754+Neclow@users.noreply.github.com>
2024-06-12 15:13:13 +0200

    add "custom" database/json_style to ResultSaver
    "custom" = burst mode + bbox coords + centroid + bbox diagonal + width/height ratio

--- deva/inference/result_utils.py
+++ deva/inference/result_utils.py
@@ -1,18 +1,22 @@
+import copy
+import math
+import os
+
+from dataclasses import dataclass
+from os import path
+from queue import Queue
+from threading import Thread
 from typing import List, Optional, Tuple, Dict
+
+import numpy as np
+import pycocotools.mask as mask_util
+import supervision as sv
 import torch
 import torch.nn.functional as F
 import torchvision
-import os
-from os import path
-from PIL import Image, ImagePalette
-import pycocotools.mask as mask_util
-from threading import Thread
-from queue import Queue
-from dataclasses import dataclass
-import copy
 
-import numpy as np
-import supervision as sv
+from scipy.ndimage import center_of_mass
+from PIL import Image, ImagePalette
 
 from deva.utils.pano_utils import ID2RGBConverter
 from deva.inference.object_manager import ObjectManager
@@ -20,13 +24,15 @@ from deva.inference.object_info import ObjectInfo
 
 
 class ResultSaver:
-    def __init__(self,
-                 output_root: str,
-                 video_name: str,
-                 *,
-                 dataset: str,
-                 object_manager: ObjectManager,
-                 palette: Optional[ImagePalette.ImagePalette] = None):
+    def __init__(
+        self,
+        output_root: str,
+        video_name: str,
+        *,
+        dataset: str,
+        object_manager: ObjectManager,
+        palette: Optional[ImagePalette.ImagePalette] = None,
+    ):
         self.output_root = output_root
         self.video_name = video_name
         self.dataset = dataset.lower()
@@ -38,42 +44,46 @@ class ResultSaver:
         self.output_postfix = None
         self.visualize = False
 
-        if self.dataset == 'vipseg':
+        if self.dataset == "vipseg":
             self.all_annotations = []
-            self.video_json = {'video_id': video_name, 'annotations': self.all_annotations}
+            self.video_json = {
+                "video_id": video_name,
+                "annotations": self.all_annotations,
+            }
             self.need_remapping = True
-            self.json_style = 'vipseg'
-            self.output_postfix = 'pan_pred'
-        elif self.dataset == 'burst':
+            self.json_style = "vipseg"
+            self.output_postfix = "pan_pred"
+        elif self.dataset == "burst" or self.dataset == "custom":
             self.id2rgb_converter = ID2RGBConverter()
             self.need_remapping = True
             self.all_annotations = []
             dataset_name = path.dirname(video_name)
             seq_name = path.basename(video_name)
             self.video_json = {
-                'dataset': dataset_name,
-                'seq_name': seq_name,
-                'segmentations': self.all_annotations
+                "dataset": dataset_name,
+                "seq_name": seq_name,
+                "segmentations": self.all_annotations,
             }
-            self.json_style = 'burst'
-        elif self.dataset == 'unsup_davis17':
+            self.json_style = dataset
+        elif self.dataset == "unsup_davis17":
             self.need_remapping = True
-        elif self.dataset == 'ref_davis':
+        elif self.dataset == "ref_davis":
             # nothing special is required
             pass
-        elif self.dataset == 'demo':
+        elif self.dataset == "demo":
             self.need_remapping = True
             self.all_annotations = []
-            self.video_json = {'annotations': self.all_annotations}
-            self.json_style = 'vipseg'
+            self.video_json = {"annotations": self.all_annotations}
+            self.json_style = "vipseg"
             self.visualize = True
-            self.visualize_postfix = 'Visualizations'
-            self.output_postfix = 'Annotations'
-        elif self.dataset == 'gradio':
+            self.visualize_postfix = "Visualizations"
+            self.output_postfix = "Annotations"
+        elif self.dataset == "gradio":
             # minimal mode, expect a cv2.VideoWriter to be assigned to self.writer asap
             self.writer = None
             self.need_remapping = True
             self.visualize = True
+
         else:
             raise NotImplementedError
 
@@ -81,23 +91,26 @@ class ResultSaver:
             self.id2rgb_converter = ID2RGBConverter()
 
         self.queue = Queue(maxsize=10)
-        self.thread = Thread(target=save_result, args=(self.queue, ))
+        self.thread = Thread(target=save_result, args=(self.queue,))
         self.thread.daemon = True
         self.thread.start()
 
-    def save_mask(self,
-                  prob: torch.Tensor,
-                  frame_name: str,
-                  need_resize: bool = False,
-                  shape: Optional[Tuple[int, int]] = None,
-                  save_the_mask: bool = True,
-                  image_np: np.ndarray = None,
-                  prompts: List[str] = None,
-                  path_to_image: str = None):
+    def save_mask(
+        self,
+        prob: torch.Tensor,
+        frame_name: str,
+        need_resize: bool = False,
+        shape: Optional[Tuple[int, int]] = None,
+        save_the_mask: bool = True,
+        image_np: np.ndarray = None,
+        prompts: List[str] = None,
+        path_to_image: str = None,
+    ):
 
         if need_resize:
-            prob = F.interpolate(prob.unsqueeze(1), shape, mode='bilinear', align_corners=False)[:,
-                                                                                                 0]
+            prob = F.interpolate(
+                prob.unsqueeze(1), shape, mode="bilinear", align_corners=False
+            )[:, 0]
         # Probability mask -> index mask
         mask = torch.argmax(prob, dim=0)
 
@@ -111,7 +124,9 @@ class ResultSaver:
             path_to_image=path_to_image,
             tmp_id_to_obj=copy.deepcopy(self.object_manager.tmp_id_to_obj),
             obj_to_tmp_id=copy.deepcopy(self.object_manager.obj_to_tmp_id),
-            segments_info=copy.deepcopy(self.object_manager.get_current_segments_info()),
+            segments_info=copy.deepcopy(
+                self.object_manager.get_current_segments_info()
+            ),
         )
 
         self.queue.put(args)
@@ -163,54 +178,120 @@ def save_result(queue: Queue):
             mask = new_mask
 
         # record output in the json file
-        if saver.json_style == 'vipseg':
+        if saver.json_style == "vipseg":
             for seg in segments_info:
-                area = int((mask == seg['id']).sum())
-                seg['area'] = area
+                area = int((mask == seg["id"]).sum())
+                seg["area"] = area
             # filter out zero-area segments
-            segments_info = [s for s in segments_info if s['area'] > 0]
+            segments_info = [s for s in segments_info if s["area"] > 0]
             # append to video level storage
             this_annotation = {
-                'file_name': frame_name[:-4] + '.jpg',
-                'segments_info': segments_info,
+                "file_name": frame_name[:-4] + ".jpg",
+                "segments_info": segments_info,
             }
             saver.all_annotations.append(this_annotation)
-        elif saver.json_style == 'burst':
+        elif saver.json_style == "burst":
             for seg in segments_info:
-                seg['mask'] = mask == seg['id']
-                seg['area'] = int(seg['mask'].sum())
-                coco_mask = mask_util.encode(np.asfortranarray(seg['mask'].numpy()))
-                coco_mask['counts'] = coco_mask['counts'].decode('utf-8')
-                seg['rle_mask'] = coco_mask
+                seg["mask"] = mask == seg["id"]
+                seg["area"] = int(seg["mask"].sum())
+                coco_mask = mask_util.encode(np.asfortranarray(seg["mask"].numpy()))
+                coco_mask["counts"] = coco_mask["counts"].decode("utf-8")
+                seg["rle_mask"] = coco_mask
             # filter out zero-area segments
-            segments_info = [s for s in segments_info if s['area'] > 0]
+            segments_info = [s for s in segments_info if s["area"] > 0]
             # append to video level storage
             this_annotation = {
-                'file_name':
-                frame_name[:-4] + '.jpg',
-                'segmentations': [{
-                    'id': seg['id'],
-                    'score': seg['score'],
-                    'rle': seg['rle_mask'],
-                } for seg in segments_info],
+                "file_name": frame_name[:-4] + ".jpg",
+                "segmentations": [
+                    {"id": seg["id"], "score": seg["score"], "rle": seg["rle_mask"]}
+                    for seg in segments_info
+                ],
+            }
+            saver.all_annotations.append(this_annotation)
+        elif saver.json_style == "custom":
+            for seg in segments_info:
+                seg["mask"] = mask == seg["id"]
+                seg["area"] = int(seg["mask"].sum())
+                coco_mask = mask_util.encode(np.asfortranarray(seg["mask"].numpy()))
+                coco_mask["counts"] = coco_mask["counts"].decode("utf-8")
+                seg["rle_mask"] = coco_mask
+
+                assert saver.object_manager.use_long_id
+                assert prompts is not None
+
+                out_mask = mask.numpy().astype(np.uint32)
+                rgb_mask = np.zeros((*out_mask.shape[-2:], 3), dtype=np.uint8)
+                for obj_id in all_obj_ids:
+                    colored_mask = saver.id2rgb_converter._id_to_rgb(obj_id)
+                    obj_mask = out_mask == obj_id
+                    rgb_mask[obj_mask] = colored_mask
+                out_img = Image.fromarray(rgb_mask)
+                alpha = (out_mask == 0).astype(np.float32) * 0.5 + 0.5
+                alpha = alpha[:, :, None]
+                blend = (image_np * alpha + rgb_mask * (1 - alpha)).astype(np.uint8)
+
+                # Add bbox information
+                # x1, y1, x2, y2  with 0 <= x1 < x2 and 0 <= y1 < y2
+                xyxy = torchvision.ops.masks_to_boxes(seg["mask"].unsqueeze(0))
+                xyxy = xyxy.numpy().squeeze()
+
+                x, y = xyxy[::2], xyxy[1::2]
+
+                seg["ul"] = (x[0], y[0])
+                seg["ur"] = (x[0], y[1])
+                seg["ll"] = (x[1], y[0])
+                seg["lr"] = (x[1], y[1])
+
+                seg["centroid"] = center_of_mass(seg["mask"].numpy())
+
+                seg["area_bbox"] = abs(x[1] - x[0]) * abs(y[1] - y[0])
+
+                seg["bbox_diag"] = math.dist(seg["ul"], seg["lr"])
+
+                seg["width_height_ratio"] = abs(y[1] - y[0]) / abs(x[1] - x[0])
+
+            # filter out zero-area segments
+            segments_info = [s for s in segments_info if s["area"] > 0]
+            # append to video level storage
+            this_annotation = {
+                "file_name": frame_name[:-4] + ".jpg",
+                "segmentations": [
+                    {
+                        "id": seg["id"],
+                        "score": seg["score"],
+                        "rle": seg["rle_mask"],
+                        "area": seg["area"],
+                        "category_id": seg["category_id"],
+                        "bbox_ul": seg["ul"],
+                        "bbox_ur": seg["ur"],
+                        "bbox_ll": seg["ll"],
+                        "bbox_lr": seg["lr"],
+                        "bbox_diag": seg["bbox_diag"],
+                        "centroid": seg["centroid"],
+                        "area_bbox": seg["area_bbox"],
+                        "area_obj_bbox_ratio": seg["area"] / seg["area_bbox"],
+                        "width_height_ratio": seg["width_height_ratio"],
+                    }
+                    for seg in segments_info
+                ],
             }
             saver.all_annotations.append(this_annotation)
         elif saver.visualize:
             # if we are visualizing, we need to preprocess segment info
             for seg in segments_info:
-                area = int((mask == seg['id']).sum())
-                seg['area'] = area
+                area = int((mask == seg["id"]).sum())
+                seg["area"] = area
             # filter out zero-area segments
-            segments_info = [s for s in segments_info if s['area'] > 0]
+            segments_info = [s for s in segments_info if s["area"] > 0]
 
         # save the mask to disk
         if save_the_mask:
             if saver.object_manager.use_long_id:
                 out_mask = mask.numpy().astype(np.uint32)
                 rgb_mask = np.zeros((*out_mask.shape[-2:], 3), dtype=np.uint8)
-                for id in all_obj_ids:
-                    colored_mask = saver.id2rgb_converter._id_to_rgb(id)
-                    obj_mask = (out_mask == id)
+                for obj_id in all_obj_ids:
+                    colored_mask = saver.id2rgb_converter._id_to_rgb(obj_id)
+                    obj_mask = out_mask == obj_id
                     rgb_mask[obj_mask] = colored_mask
                 out_img = Image.fromarray(rgb_mask)
             else:
@@ -219,7 +300,7 @@ def save_result(queue: Queue):
                 if saver.palette is not None:
                     out_img.putpalette(saver.palette)
 
-            if saver.dataset != 'gradio':
+            if saver.dataset != "gradio":
                 # find a place to save the mask
                 if saver.output_postfix is not None:
                     this_out_path = path.join(saver.output_root, saver.output_postfix)
@@ -229,14 +310,16 @@ def save_result(queue: Queue):
                     this_out_path = path.join(this_out_path, saver.video_name)
 
                 os.makedirs(this_out_path, exist_ok=True)
-                out_img.save(path.join(this_out_path, frame_name[:-4] + '.png'))
+                out_img.save(path.join(this_out_path, frame_name[:-4] + ".png"))
 
             if saver.visualize and saver.object_manager.use_long_id:
                 if image_np is None:
                     if path_to_image is not None:
                         image_np = np.array(Image.open(path_to_image))
                     else:
-                        raise ValueError('Cannot visualize without image_np or path_to_image')
+                        raise ValueError(
+                            "Cannot visualize without image_np or path_to_image"
+                        )
                 alpha = (out_mask == 0).astype(np.float32) * 0.5 + 0.5
                 alpha = alpha[:, :, None]
                 blend = (image_np * alpha + rgb_mask * (1 - alpha)).astype(np.uint8)
@@ -248,37 +331,46 @@ def save_result(queue: Queue):
                     all_cat_ids = []
                     all_scores = []
                     for seg in segments_info:
-                        all_masks.append(mask == seg['id'])
-                        labels.append(f'{prompts[seg["category_id"]]} {seg["score"]:.2f}')
-                        all_cat_ids.append(seg['category_id'])
-                        all_scores.append(seg['score'])
+                        all_masks.append(mask == seg["id"])
+                        labels.append(
+                            f'{prompts[seg["category_id"]]} {seg["score"]:.2f}'
+                        )
+                        all_cat_ids.append(seg["category_id"])
+                        all_scores.append(seg["score"])
                     if len(all_masks) > 0:
                         all_masks = torch.stack(all_masks, dim=0)
                         xyxy = torchvision.ops.masks_to_boxes(all_masks)
                         xyxy = xyxy.numpy()
 
-                        detections = sv.Detections(xyxy,
-                                                   confidence=np.array(all_scores),
-                                                   class_id=np.array(all_cat_ids))
+                        detections = sv.Detections(
+                            xyxy,
+                            confidence=np.array(all_scores),
+                            class_id=np.array(all_cat_ids),
+                        )
                         box_annotator = sv.BoundingBoxAnnotator()
                         label_annotator = sv.LabelAnnotator()
-                        blend = box_annotator.annotate(scene=blend,
-                                                   detections=detections)
-                        blend = label_annotator.annotate(scene=blend,
-                                                        detections=detections,
-                                                        labels=labels)
+                        blend = box_annotator.annotate(
+                            scene=blend, detections=detections
+                        )
+                        blend = label_annotator.annotate(
+                            scene=blend, detections=detections, labels=labels
+                        )
 
-                if saver.dataset != 'gradio':
+                if saver.dataset != "gradio":
                     # find a place to save the visualization
                     if saver.visualize_postfix is not None:
-                        this_out_path = path.join(saver.output_root, saver.visualize_postfix)
+                        this_out_path = path.join(
+                            saver.output_root, saver.visualize_postfix
+                        )
                     else:
                         this_out_path = saver.output_root
                     if saver.video_name is not None:
                         this_out_path = path.join(this_out_path, saver.video_name)
 
                     os.makedirs(this_out_path, exist_ok=True)
-                    Image.fromarray(blend).save(path.join(this_out_path, frame_name[:-4] + '.jpg'))
+                    Image.fromarray(blend).save(
+                        path.join(this_out_path, frame_name[:-4] + ".jpg")
+                    )
                 else:
                     saver.writer.write(blend[:, :, ::-1])
 
